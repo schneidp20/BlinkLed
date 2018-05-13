f696ea9efb48ac1404f0b8828fe96a37a8e8f1fb
david schneider <david.p.schneider@gmail.com>
2018-05-13 16:26:47 -0500

    initial checkin
    initial checkin

---
+++ BlinkLed.cpp
@@ -0,0 +1,75 @@
+#include <Adafruit_NeoPixel.h>
+#include <ArrayConstants.h> // WARNING: check these constants before compiling, it may need changing between sketches
+#include "BlinkLed.h"
+
+BlinkLed::BlinkLed(Adafruit_NeoPixel *strip, byte(*pix)[MAXSINGLEROW], byte pixmax, byte red, byte green, byte blue, long freq)
+{	
+	_strip = strip;
+	_pix = pix;      
+	_pixmax = pixmax;	
+	_red = red;
+	_green = green;
+	_blue = blue;		
+	_freq = freq;
+	
+	_currentlyon = false;
+	_blinkon = false;
+	_nextIteration = 0 ;
+}
+
+void BlinkLed::Blink(bool on)
+{ 
+  if (on)
+  {
+    _currentlyon = true;
+    long now = millis();
+    if (_nextIteration < now)
+    {
+		_nextIteration = now + _freq;
+		set(!_blinkon); // set to opposite of its current state
+    }
+  }
+  else
+  {
+    if (_currentlyon) // only turn off if it's currently on
+	{
+		_currentlyon = false;
+		set(false); // turn off
+	}
+  }
+}
+
+void BlinkLed::set(bool on)
+{
+	_blinkon= on;
+	for (int x = 0; x<_pixmax; x++)
+	{
+		if (_blinkon)
+		{     
+			_strip->setPixelColor((*_pix)[x], _strip->Color(_red, _green, _blue));
+		}
+		else
+		{
+			_strip->setPixelColor((*_pix)[x], _strip->Color(0,0,0));
+		}
+	}
+	_strip->show();
+}
+
+void BlinkLed::Set(bool on)
+{
+	for (int x = 0; x<_pixmax; x++)
+	{
+		if (on)
+		{     
+			_strip->setPixelColor((*_pix)[x], _strip->Color(_red, _green, _blue));
+		}
+		else if (_currentlyon) // only turn off if it's currently on
+		{
+			_strip->setPixelColor((*_pix)[x], _strip->Color(0,0,0));
+		}
+	}
+	_strip->show();
+	_currentlyon = on;
+}
+
---
+++ BlinkLed.h
@@ -0,0 +1,32 @@
+#include <Adafruit_NeoPixel.h>
+
+class BlinkLed{
+
+public:
+	BlinkLed(Adafruit_NeoPixel *strip, byte(*pix)[MAXSINGLEROW], byte pixmax, byte red, byte green, byte blue, long freq);
+	
+	// Call this on each loop iteration and Blink will automatically do the blink frequency.
+	// on - true, the method will do the blink frequency
+	//      false, turn off the LEDs. 
+	void Blink(bool on);
+
+	// Just turn on/off the LEDs w/o blink frequency.   
+	void Set(bool on);
+
+
+
+private:
+	void set(bool on);
+	Adafruit_NeoPixel *_strip;
+	byte (*_pix)[MAXSINGLEROW];
+	byte _pixmax;
+	byte _red;	
+	byte _green;
+	byte _blue;
+	long _freq;
+
+	bool _currentlyon;
+	bool _blinkon;
+	long _nextIteration;
+	
+};
